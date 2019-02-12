b9da1f4f1e517c91015717bfc5383c8c238a4711
alwaysbespoke <33767957+alwaysbespoke@users.noreply.github.com>
2019-02-12 15:23:18 -0800

    Add files via upload

--- lhventures.py
+++ lhventures.py
@@ -1,45 +1,126 @@
 import csv
 
+CSV_EXT = ".csv"
+CSV_BASE = "csv/"
+COMMA = ","
+
 class SQLInMemory:
 
 	def __init__(self, paths):
 		print("SQLInMemory")
 
-		self.result = "INSERT RESULT"
+		self.result = []
+
+		self.tables = {}
 
 		for path in paths:
+
 			self.openCSV(path)
 
 	def openCSV(self, path):
 		print("SQLInMemory -> openCSV()")
 
-		with open("csv/"+path+".csv") as csvfile:
+		with open(CSV_BASE + path + CSV_EXT) as csvfile:
 
-			rows = csv.reader(csvfile, delimiter=",")
+			rows = csv.reader(csvfile, delimiter = COMMA)
 
 			table = []
 
+			fieldsByName = {}
+
+			fieldsByIndex = []
+
+			first = True
+
 			for row in rows:
+
+				if first:
+
+					first = False
+
+					i = 0
+
+					for field in row:
+
+						fieldsByName[field] = i
+
+						fieldsByIndex.append(field)
+
+						i += 1
+
 				table.append(row)
+			
+			obj = {"table":table, "fieldsByName":fieldsByName, "fieldsByIndex":fieldsByIndex}
 
-			if path == "Restaurants":
-				self.restaurants = table
-			else:
-				self.ratings = table
+			self.tables[path] = obj	
 
-			print(table)	
+	def Select(self, fields):
+		print("SQLInMemory -> Select()")
+		self.fieldsToSelectByName = fields
 
-	def select(self, fields):
-		print("SQLInMemory -> select()")
-		self.selectQ = fields
+	def From(self, tables):
+		print("SQLInMemory -> From()")
+		self.fromTables = tables
 
-	def fromR(self, tables):
-		print("SQLInMemory -> from()")
-		self.tablesQ = tables
+	def Where(self, lookUps):
+		print("SQLInMemory -> Where()")
 
-	def where(self, locations):
-		print("SQLInMemory -> where()")
-		
+		for tableName in self.fromTables:
+
+			obj = self.tables[tableName]
+
+			table = obj["table"]
+
+			fieldsByName = obj["fieldsByName"]
+
+			fieldsByIndex = obj["fieldsByIndex"]
+
+			fieldsToSelectByIndex = self.getFieldsToSelectByIndex(fieldsByName)
+
+			for row in table:
+				
+				#loop through WHERE cases
+				for fieldName in lookUps:
+
+					lookUp = lookUps[fieldName]
+
+					fieldIndex = fieldsByName[fieldName]
+
+					fieldVal = row[fieldIndex]
+
+					#check if WHERE case is true
+					if fieldVal == lookUp:
+
+						result = self.getResult(fieldsToSelectByIndex, fieldsByIndex, row)
+						
+						#add the result to result
+						self.result.append(result)
+
+	def getResult(self, fieldsToSelectByIndex, fieldsByIndex, row):
+
+		result = {}
+
+		#if WHERE case is true grab the field data specified in SELECT
+		for fieldIndex in fieldsToSelectByIndex:
+
+			fieldName = fieldsByIndex[fieldIndex]
+
+			result[fieldName] = row[fieldIndex]
+
+		return result
+
+
+	def getFieldsToSelectByIndex(self, fieldsByName):
+
+		fieldsToSelectByIndex = []
+
+		for fieldName in self.fieldsToSelectByName:
+
+				fieldIndex = fieldsByName[fieldName]
+
+				fieldsToSelectByIndex.append(fieldIndex)
+
+		return fieldsToSelectByIndex
 
 
 class SQLOnDisk:
@@ -51,4 +132,7 @@ class SQLOnDisk:
 
 #instantiate SQLInMemory
 sql = SQLInMemory(["Restaurants", "Ratings"])
+sql.From(["Restaurants"])
+sql.Select(["name"])
+sql.Where({"country":"Spain"})
 print(sql.result)
